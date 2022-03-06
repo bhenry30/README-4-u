c2ca096edca1a69063fab855a4ab7223033a47af
Brandon Henry <brandonhenry@Ginas-iMac.lan>
2022-03-06 15:17:38 -0600

    license functions made and implented

---
+++ Develop/README.md
@@ -0,0 +1,43 @@
+
+  # a
+
+ **Table of Contents**
+
+ 1. [Description](#description)
+ 2. [Installation Instructions](#installation-instructions)
+ 3. [Usage Information](#usage-information)
+ 4. [Contribution Guidelines](#contribution-guidelines)
+ 5. [Test Instructions](#test-instructions)
+ 6. [License](#license)
+ 7. [Questions](#questions)
+  
+  <a name="description"></a>
+  ## Description 
+  d
+
+  <a name="installation-instructions"></a>
+  ## Installation Instructions
+  e
+
+  <a name="usage-information"></a>
+  ## Usage Information
+  r
+
+  <a name="contribution-guidelines"></a>
+  ## Contribution Guidelines
+  q
+
+  <a name="test-instructions"></a>
+  ## Test Instructions
+  e
+
+  
+  <a name="license"></a>
+  ## License
+  ![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)[Apache License 2.0](![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg))
+  
+
+  <a name="questions"></a>
+  ## Questions <a name="questions"></a>
+  - GitHub profile: https://github.com/f
+  - Contact me here if you have any questions: d
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,14 +1,44 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
+function renderLicenseBadge(license) {
+  if (!license) {
+    return ''
+  } else if (license = 'Apache License 2.0'){
+    return 'https://opensource.org/licenses/Apache-2.0'
+  } else if (license = 'GNU General Public License'){
+    return 'https://opensource.org/licenses/gpl-license'
+  } else if (license = 'Mozilla Public License 2.0'){
+      return 'https://opensource.org/licenses/MPL-2.0'  
+  
+}
+}
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (!license) {
+    return '';
+  } else if (license = 'Apache License 2.0'){
+    return '![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)'
+  } else if (license = 'GNU General Public License'){
+    return '![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)'
+  } else if (license = 'Mozilla Public License 2.0'){
+      return '![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)'  
+}
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  // const { license } = license
+  if (!license) {
+    return ''
+  }
+  return `
+  <a name="license"></a>
+  ## License
+  ${renderLicenseLink(license)} [${license}](${renderLicenseLink(license)})
+  `
+}
 
 // TODO: Create a function to generate markdown for README
 const generateMarkdown = templateData => {
@@ -47,9 +77,7 @@ const generateMarkdown = templateData => {
   ## Test Instructions
   ${test}
 
-  <a name="license"></a>
-  ## License
-  ${license}
+  ${renderLicenseSection(license)}
 
   <a name="questions"></a>
   ## Questions <a name="questions"></a>
